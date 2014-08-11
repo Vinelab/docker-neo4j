d8672bfb5359e08496bb7ae47e3634f57e61564c
Abed Halawi <halawi.abed@gmail.com>
2014-08-11 21:24:53 +0300

    fix dirty and writeback pages problem according to docs: http://docs.neo4j.org/chunked/stable/linux-performance-guide.html#_fixing_the_problem

--- launch.sh
+++ launch.sh
@@ -5,6 +5,11 @@ export JAVA_HOME=/usr/lib/jvm/jre-openjdk/
 IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$IPADDR|g" /var/lib/neo4j/conf/neo4j-server.properties
 
+# Fix dirty and writeback pages according to: http://docs.neo4j.org/chunked/stable/linux-performance-guide.html#_fixing_the_problem
+echo 'vm.dirty_background_ratio = 50' >> /etc/sysctl.conf
+echo 'vm.dirty_ratio = 80' >> /etc/sysctl.conf
+sysctl -p
+
 # Increase ulimit
 ulimit -n 65536
 # Run supervisor
